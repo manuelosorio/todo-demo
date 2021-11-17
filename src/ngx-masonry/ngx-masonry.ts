2d19931d17fb0020a9f417139e974426cc28dab9
manuelosorio <manuelosorio.mkdesignz@gmail.com>
2021-11-17 13:15:59 -0500

    1 file modified, 5 added

---
+++ src/ngx-masonry/ngx-masonry.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of ngx-masonry
+ */
+
+export * from './ngx-masonry.component';
+export * from './ngx-masonry.directive';
+export * from './ngx-masonry-options';
+export * from './ngx-masonry.module';
