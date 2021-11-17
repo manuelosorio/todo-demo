7ea9a10a3205a27cc4ef46eec6c24bfa4b8e9301
manuelosorio <manuelosorio.mkdesignz@gmail.com>
2021-11-17 11:50:40 -0500

    2 files added

---
+++ src/app/fake-list/fake-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fake-list',
+  templateUrl: './fake-list.component.html',
+  styleUrls: ['./fake-list.component.css'],
+})
+export class FakeListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
